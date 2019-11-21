79f4e2fa708089b21d2180919d35fe82cb264f69
gnd <gnd@itchybit.org>
2019-11-21 20:30:32 +0100

    back to basics, writing a SDF shader from scratch - fixing lightning, adding faster normal(p), adding gamma

--- basic_scene_2019.glsl
+++ basic_scene_2019.glsl
@@ -1,12 +1,15 @@
+#version 460
+out vec4 PixelColor;
 uniform vec2 resolution;
 uniform float time;
 uniform sampler2D backbuffer;
 #define MAXSTEPS 128
 #define MAXDIST 20.0
+#define eps 0.0001
 #define SINBUMPS
-//#define REPEAT
-//#define REPEATPLANE
-//#define FLY
+##define REPEAT
+##define REPEATPLANE
+##define FLY
 
 struct light { vec3 position; vec3 color; };
 struct material { vec3 color; float reflection_ratio; float shininess; };
@@ -63,8 +66,6 @@ float scene_plane(vec3 p) {
 
 float scene_object(vec3 p) {
     #ifdef REPEAT
-        // if doing domain repeat it is vital to avoid problems with zero-centered objects
-        // push the domain by half of modulo to the side after mod
         p.x = mod(p.x,1.) - .5;
         p.z = mod(p.z,2.) - 1.;
     #endif
@@ -72,12 +73,6 @@ float scene_object(vec3 p) {
 }
 
 float glass_sphere(vec3 p) {
-    #ifdef REPEAT
-        // if doing domain repeat it is vital to avoid problems with zero-centered objects
-        // push the domain by half of modulo to the side after mod
-        p.x = mod(p.x,1.) - .5;
-        p.z = mod(p.z,2.) - 1.;
-    #endif
     return sphere(p-vec3(0.,1.,.0), .5);
 }
 
@@ -85,24 +80,16 @@ float scene(vec3 p) {
     return min(min(scene_plane(p), scene_object(p)), glass_sphere(p));
 }
 
-// aproximate surface normal by actually doing a difference between:
-//  A' = A + dx; A'' = A - dx;
-// dist(A') - dist(A) - (dist(A'') - dist(A))
-// = dist(A') - dist(A'')
-// the resulting vector is pointing/growing in the direction of the normal
-vec3 normal(vec3 p, float dx) {
-    vec3 n = vec3(0.);
-    vec3 dxx = vec3(1.,.0,.0)*dx;
-    vec3 dxy = vec3(0.,1.,0.)*dx;
-    vec3 dxz = vec3(0.,0.,1.)*dx;
-    n.x = (scene(p + dxx) - scene(p - dxx));
-    n.y = (scene(p + dxy) - scene(p - dxy));
-    n.z = (scene(p + dxz) - scene(p - dxz));
-    return normalize(n);
+// taken from http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+vec3 normal(vec3 p) {
+    const vec2 k = vec2(1,-1);
+    return normalize( k.xyy*scene( p + k.xyy*eps ) +
+                      k.yyx*scene( p + k.yyx*eps ) +
+                      k.yxy*scene( p + k.yxy*eps ) +
+                      k.xxx*scene( p + k.xxx*eps ) );
 }
 
-float fresnel(vec3 V, vec3 N, float R0)
-{
+float fresnel(vec3 V, vec3 N, float R0) {
     float cosAngle = 1.0-max(dot(V, N), 0.0);
     float result = cosAngle * cosAngle;
     result = result * result;
@@ -115,7 +102,7 @@ shading get_shading(material m, light l, vec3 p, vec3 eye) {
     shading s;
     float step;
     vec3 light_dir = l.position-p; // direction from p to light
-    vec3 normal = normal(p, 0.001); // surface normal at point p
+    vec3 normal = normal(p); // surface normal at point p
 
     // Diffuse shading using Lambertian reflectance
     // https://en.wikipedia.org/wiki/Lambertian_reflectance
@@ -123,8 +110,8 @@ shading get_shading(material m, light l, vec3 p, vec3 eye) {
 
     // Specular reflections using phong
     // https://en.wikipedia.org/wiki/Phong_reflection_model
-    vec3 reflection_dir = 2.*dot(normalize(light_dir), normal)*normal-normalize(light_dir);
-    s.specular = clamp(m.reflection_ratio * pow(dot(normalize(reflection_dir), normalize(eye-p)), m.shininess), 0., 1.);
+    vec3 reflection_dir = normalize(reflect(-light_dir, normal));
+    s.specular = clamp(m.reflection_ratio * pow(dot(reflection_dir, normalize(eye-p)), m.shininess), 0., 1.);
 
     // Determine intensity of light at p
     float light_dist = scene(l.position);
@@ -134,19 +121,19 @@ shading get_shading(material m, light l, vec3 p, vec3 eye) {
     // Shadows - basically raymarch towards the light
     // If the path from p to each light source is obstructed add shadow
     s.shadow = 1.;
-    step = 0.1;
-    for (int i = 0; i < 20; i++ ) {
+    step = 0.0025;
+    for (int i = 0; i < MAXSTEPS/3; i++ ) {
         vec3 shadow_p = p + normalize(light_dir) * step;
         float shadow_d = scene(shadow_p);
         if (shadow_d > length(light_dir)) {
             break;
         }
-        if (shadow_d < 0.001) {
+        if ((shadow_d < eps) || (step > MAXDIST*2)) {
             s.shadow = 0.;
             break;
         }
         // check http://www.iquilezles.org/www/articles/rmshadows/rmshadows.htm
-        s.shadow = min( s.shadow, 10.*shadow_d/step );
+        s.shadow = min( s.shadow, 32.*shadow_d/step );
         step += shadow_d;
     }
     s.shadow = clamp(s.shadow, 0., 1.);
@@ -158,33 +145,40 @@ shading get_shading(material m, light l, vec3 p, vec3 eye) {
     for (int k =0; k < 5; k++) {
         step += .04;
         vec3 aoc_p = p + normal * step;
-        float diff = .9*abs(length(aoc_p-p) - scene(aoc_p));
+        float diff = 1.5*abs(length(aoc_p-p) - scene(aoc_p));
         s.aoc = min(s.aoc, s.aoc-diff);
     }
 
     // Ambient light
-    s.amb = clamp(0.5+0.5*normal.y, 0., 1.);
+    s.amb = clamp(0.5+0.4*normal.y, 0., 1.);
+
     return s;
 }
 
 void main() {
     #ifdef FLY
-        vec3 eye = vec3(cos(time/3.), sin(time/3.)*3., time); // camera position
-        l1.position = vec3(cos(time)*10.,2.,sin(time)*10.+time); // light position
-        vec3 fwd = normalize(vec3(0.,0.,7.+time)-eye);
+        vec3 eye = vec3(cos(time/3.), sin(time/3.)*3., time);
+        vec3 lookat  = vec3(0.,0.,7.+time);
+        l1.position = vec3(cos(time)*10.,2.,sin(time)*10.+time);
     #else
-        vec3 eye = vec3(cos(time/3.), sin(time/3.)*0. + 2., -3.); // camera position
+        vec3 eye = vec3(cos(time/3.)*3., sin(time/3.)*0. + 2., sin(time/3.)*3.);
+        vec3 lookat = vec3(0.,0.,0.);
         l1.position = vec3(cos(time)*3.,2.,sin(time)*3.); // light position
-        vec3 fwd = normalize(vec3(0.)-eye);
     #endif
-    vec3 up = vec3(0, 1, 0);
-    vec3 right = vec3(1, 0, 0);
-    l1.color = vec3(1.,1.,1.); // light color
+
+    vec3 fwd = normalize(lookat-eye);
+    vec3 right = normalize(vec3(fwd.z, 0., -fwd.x ));
+    vec3 up = normalize(cross(fwd, right));
     float u = gl_FragCoord.x * 2.0 / resolution.x - 1.0;
     float v = gl_FragCoord.y * 2.0 / resolution.y - 1.0;
-    vec3 rd = normalize(fwd + right*u + up*v/1.77); // 'ray distance'
-    vec3 ro = eye; // 'ray origin'
-    vec3 color = vec3(.0); // 'sky color'
+    float aspect = resolution.x/resolution.y;
+    vec3 ro = eye;
+    vec3 rd = normalize(1.1*fwd + u*right*aspect + v*up);
+
+    // enviroment
+    l1.color = vec3(1.,1.,1.);
+    //l1.position = vec3(cos(time)*3.,2.,sin(time)*3.); // light position
+    vec3 color = vec3(0.,0.,0.); // 'sky color'
 
     // raymarch a scene
     float step = .0;
@@ -193,14 +187,10 @@ void main() {
         float d = scene(p);
         // if ray doesnt hit any surface, kill it after being longer than MAXDIST
         if (d > MAXDIST) {
-            //color += vec3(0.8);
             break;
         }
         // if ray very close to a surface, find out how to color that surface AKA what color is the pixel
-        if (d < 0.001) {
-            // red haze - basically just the number of iterations to reach the surface
-            color = vec3(float(i)*(1./float(MAXDIST)), 0.,0.)*.3;
-
+        if (d < eps) {
             // shading the plane
             if (scene_plane(p) == scene(p)) {
                 m2.color = vec3(0.,0.,1.)*10.;
@@ -214,36 +204,40 @@ void main() {
             }
             // shading the object
             if (scene_object(p) == scene(p)) {
-                m1.color = vec3(.1,0.,0.);
-                m1.reflection_ratio = 1.9;
+                m1.color = vec3(.0,0.,0.);
+                m1.reflection_ratio = .9;
                 m1.shininess = 10.9;
                 s1 = get_shading(m1, l1, p, eye);
                 // check https://www.shadertoy.com/view/lsKcDD
-                color += m1.color * s1.diffuse * s1.intensity *s1.shadow * 1.;
+                color = color * 0.9 + m1.color * s1.diffuse;// * s1.intensity *s1.shadow * .1;
                 color += s1.specular;
                 color += m1.color * s1.aoc * s1.amb * 0.2;
             }
             // shading the glass sphere
             if (glass_sphere(p) == scene(p)) {
-                m1.color = vec3(0.1,0.1,0.1);
+                m1.color = vec3(0.0);
                 m1.reflection_ratio = 10.9;
                 m1.shininess = 100.9;
                 s1 = get_shading(m1, l1, p, eye);
                 // check https://www.shadertoy.com/view/lsKcDD
-                float fr = fresnel(normalize(eye-p), normal(p,0.0001), 0.5);
-                vec3 reflection = fr * texture(backbuffer, reflect(-1.*normalize(eye-p), normal(p, 0.0001))).xyz;
-                vec3 refraction = (1.0-fr) * texture(backbuffer, refract(-1.*normalize(eye-p), normal(p, 0.0001), 0.8)).xyz;
-    	        color += reflection + refraction;
-                color += m1.color * s1.diffuse * s1.intensity;// *s1.shadow * 1.;
-                color += s1.specular;
+                float fr = fresnel(normalize(eye-p), normal(p), 0.5);
+                vec3 reflection = fr * texture(backbuffer, reflect(-1.*normalize(eye-p), normal(p)).xy).xyz;
+                vec3 refraction = (1.0-fr) * texture(backbuffer, refract(-1.*normalize(eye-p), normal(p), 0.8).xy).xyz;
+                color *= m1.color;
+    	        color += (reflection + refraction) * .5;
+                //color += m1.color * s1.diffuse * s1.intensity * 0.01;// *s1.shadow * 1.;
+                color += s1.specular * 2;
                 //color += m1.color * s1.aoc * s1.amb * 0.2;
             }
             break;
         }
         step += d;
         // red haze makes it lil bit interesting
-        color = vec3(float(i)*(1./32.), 0.,0.)*.3;
+        color += vec3(float(i)*(1./float(MAXSTEPS/3)), 0.,0.)*.01;
     }
 
-    gl_FragColor = vec4(color, 0.);
+    // gamma correction
+    color = pow( color, vec3(1.0/2.2) );
+
+    PixelColor = vec4(color, 0.);
 }
