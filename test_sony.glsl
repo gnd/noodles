1f372194c8ec6b729a74a50b6acdead1cefdd25f
gnd <gnd@itchybit.org>
2019-11-28 00:14:39 +0100

    changing gl_FragColor to custom out var

--- test_sony.glsl
+++ test_sony.glsl
@@ -1,7 +1,9 @@
+#version 330
+out vec4 PixelColor;
 uniform vec2 resolution;
 uniform sampler2D sony;
 
 void main(void) {
 	vec2 p = vec2( gl_FragCoord.x / resolution.x, 1.0 - gl_FragCoord.y / resolution.y);
-	gl_FragColor = texture2D(sony, p);
+	PixelColor = texture2D(sony, p);
 }
