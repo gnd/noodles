46bfe37f76f6dccf4b9d74e5992a00b7b2654aee
gnd <gnd@itchybit.org>
2019-12-03 17:36:35 +0100

    sandbox

---
+++ deform_by_sound.glsl
@@ -0,0 +1,419 @@
+#version 330
+out vec4 PixelColor;
+uniform vec2 resolution;
+uniform float time;
+#define MAXSTEPS 128
+#define MAXDIST 20.0
+#define eps 0.0001
+#define PI 3.14159265
+#define TAU (2*PI)
+#define PHI (sqrt(5)*0.5 + 0.5)
+
+struct light { vec3 position; vec3 color; };
+struct material { vec3 color; float reflection_ratio; float shininess; };
+struct shading { float diffuse; float specular; float shadow; float aoc; float amb; };
+
+light l1;
+material mat1;
+shading s1;
+
+vec3 sky, color; // 'sky color'
+
+/// PARAMS
+uniform float m0;
+uniform float m1;
+uniform float m2;
+uniform float m3;
+uniform float m4;
+uniform float m5;
+uniform float m6;
+uniform float m7;
+
+/// COLORS
+vec3 cx;
+vec3 ck = vec3(0.);
+vec3 cw = vec3(1.0);
+vec3 cr = vec3(1.,0.,0.);
+vec3 cg = vec3(0.,1.,0.);
+vec3 cb = vec3(0.,0.,1.);
+//////////////////// END OF GLOBALS //////////////
+
+/////////////////// MIDIMIX //////////////////////
+uniform float m8,m9,m10;
+uniform float m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m21,m22,m23,m24,m25,m26,m27,m28,m29,m30;
+uniform float m31,m32,m33,m34,m35,m36,m37,m38,m39,m40,m41,m42,m43,m44,m45,m46,m47,m48,m49,m50;
+uniform float m51,m52,m53,m54,m55,m56,m57,m58,m59;
+float mx11 = m8/127.;
+float mx12 = m9/127.;
+float mx13 = m10/127.;
+float mx14 = m11/127.;
+float mx15 = m12/127.;
+float mx16 = m13/127.;
+float mx21 = m14/127.;
+float mx22 = m15/127.;
+float mx23 = m16/127.;
+float mx24 = m17/127.;
+float mx25 = m18/127.;
+float mx26 = m19/127.;
+float mx31 = m20/127.;
+float mx32 = m21/127.;
+float mx33 = m22/127.;
+float mx34 = m23/127.;
+float mx35 = m24/127.;
+float mx36 = m25/127.;
+float mx41 = m26/127.;
+float mx42 = m27/127.;
+float mx43 = m28/127.;
+float mx44 = m29/127.;
+float mx45 = m30/127.;
+float mx46 = m31/127.;
+float mx51 = m32/127.;
+float mx52 = m33/127.;
+float mx53 = m34/127.;
+float mx54 = m35/127.;
+float mx55 = m36/127.;
+float mx56 = m37/127.;
+float mx61 = m38/127.;
+float mx62 = m39/127.;
+float mx63 = m40/127.;
+float mx64 = m41/127.;
+float mx65 = m42/127.;
+float mx66 = m43/127.;
+float mx71 = m44/127.;
+float mx72 = m45/127.;
+float mx73 = m46/127.;
+float mx74 = m47/127.;
+float mx75 = m48/127.;
+float mx76 = m49/127.;
+float mx81 = m50/127.;
+float mx82 = m51/127.;
+float mx83 = m52/127.;
+float mx84 = m53/127.;
+float mx85 = m54/127.;
+float mx86 = m55/127.;
+float mx91 = m56/127.;
+float mx92 = m57/127.;
+float mx93 = m58/127.;
+float mx94 = m59/127.;
+///////////////// END OF MIDIMIX ////////////////////
+
+
+
+
+/////////////////////////////////////////// GEOMETRIC MODIFIERS
+mat4 inverse(mat4 m) {
+  float
+      a00 = m[0][0], a01 = m[0][1], a02 = m[0][2], a03 = m[0][3],
+      a10 = m[1][0], a11 = m[1][1], a12 = m[1][2], a13 = m[1][3],
+      a20 = m[2][0], a21 = m[2][1], a22 = m[2][2], a23 = m[2][3],
+      a30 = m[3][0], a31 = m[3][1], a32 = m[3][2], a33 = m[3][3],
+
+      b00 = a00 * a11 - a01 * a10,
+      b01 = a00 * a12 - a02 * a10,
+      b02 = a00 * a13 - a03 * a10,
+      b03 = a01 * a12 - a02 * a11,
+      b04 = a01 * a13 - a03 * a11,
+      b05 = a02 * a13 - a03 * a12,
+      b06 = a20 * a31 - a21 * a30,
+      b07 = a20 * a32 - a22 * a30,
+      b08 = a20 * a33 - a23 * a30,
+      b09 = a21 * a32 - a22 * a31,
+      b10 = a21 * a33 - a23 * a31,
+      b11 = a22 * a33 - a23 * a32,
+
+      det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+  return mat4(
+      a11 * b11 - a12 * b10 + a13 * b09,
+      a02 * b10 - a01 * b11 - a03 * b09,
+      a31 * b05 - a32 * b04 + a33 * b03,
+      a22 * b04 - a21 * b05 - a23 * b03,
+      a12 * b08 - a10 * b11 - a13 * b07,
+      a00 * b11 - a02 * b08 + a03 * b07,
+      a32 * b02 - a30 * b05 - a33 * b01,
+      a20 * b05 - a22 * b02 + a23 * b01,
+      a10 * b10 - a11 * b08 + a13 * b06,
+      a01 * b08 - a00 * b10 - a03 * b06,
+      a30 * b04 - a31 * b02 + a33 * b00,
+      a21 * b02 - a20 * b04 - a23 * b00,
+      a11 * b07 - a10 * b09 - a12 * b06,
+      a00 * b09 - a01 * b07 + a02 * b06,
+      a31 * b01 - a30 * b03 - a32 * b00,
+      a20 * b03 - a21 * b01 + a22 * b00) / det;
+}
+
+
+mat4 rmat(vec3 axis, float angle)
+{
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+    float oc = 1.0 - c;
+
+    return mat4(oc * axis.x * axis.x + c, oc * axis.x * axis.y - axis.z * s, oc * axis.z * axis.x + axis.y * s, 0.0,
+                oc * axis.x * axis.y + axis.z * s, oc * axis.y * axis.y + c, oc * axis.y * axis.z - axis.x * s, 0.0,
+                oc * axis.z * axis.x - axis.y * s, oc * axis.y * axis.z + axis.x * s, oc * axis.z * axis.z + c, 0.0,
+                0.0, 0.0, 0.0, 1.0);
+}
+
+vec3 rot( vec3 p, mat4 m )
+{
+    vec4 q = inverse(m)*vec4(p,1.);
+    return vec3(q.x,q.y,q.z);
+}
+
+vec3 twist(in vec3 p, float r)
+{
+    float k = r*100.; // or some other amount
+    float c = cos(k*p.y);
+    float s = sin(k*p.y);
+    mat2  m = mat2(c,-s,s,c);
+    vec3  q = vec3(m*p.xz,p.y);
+    return vec3(q.x,q.z,q.y); // we need this to fix the twist
+}
+
+vec3 xtwist(in vec3 p, float r)
+{
+    float k = r; // or some other amount
+    float c = cos(k*p.x);
+    float s = sin(k*p.x);
+    mat2  m = mat2(c,-s,s,c);
+    vec3  q = vec3(m*p.yz,p.x);
+    return vec3(q.z,q.x,q.y); // we need this to fix the twist
+}
+
+// deformed twist
+vec3 dtwist(in vec3 p)
+{
+    float k = 0.1+mx11*100.; // or some other amount
+    float c = cos(k*p.y);
+    float s = sin(k*p.y);
+    mat2  m = mat2(c,-s,s,c);
+    vec3  q = vec3(m*p.xz,p.z);
+    return q;
+}
+
+// Repeat around the origin by a fixed angle.
+// For easier use, num of repetitions is use to specify the angle.
+float modp(inout vec2 p, float repetitions) {
+	float angle = 2*PI/repetitions;
+	float a = atan(p.y, p.x) + angle/2.;
+	float r = length(p);
+	float c = floor(a/angle);
+	a = mod(a,angle) - angle/2.;
+	p = vec2(cos(a), sin(a))*r;
+	// For an odd number of repetitions, fix cell index of the cell in -x direction
+	// (cell index would be e.g. -5 and 5 in the two halves of the cell):
+	if (abs(c) >= (repetitions/2)) c = abs(c);
+	return c;
+}
+
+float blend(in float d1, in float d2, in float k) {
+	return k * d1 + (1.-k) * d2;
+}
+
+float smin( float a, float b, float k ) {
+    float res = exp( -k*a ) + exp( -k*b );
+    return -log( res )/k;
+}
+
+
+
+float sphere( vec3 p, float r ) {
+    return length(p) - r;
+}
+
+float maxcomp(in vec3 p ) {
+    return max(p.x,max(p.y,p.z));
+}
+
+float box(vec3 p, vec3 b, float r) {
+    vec3 d = abs(p) - b;
+    return min(maxcomp(d),0.0) - r + length(max(d,0.0));
+}
+
+// Blobby ball object. You've probably seen it somewhere. This is not a correct distance bound, beware.
+float fblob(vec3 p) {
+	p = abs(p);
+	if (p.x < max(p.y, p.z)) p = p.yzx;
+	if (p.x < max(p.y, p.z)) p = p.yzx;
+	float b = max(max(max(
+		dot(p, normalize(vec3(1, 1, 1))),
+		dot(p.xz, normalize(vec2(PHI+1, 1)))),
+		dot(p.yx, normalize(vec2(1, PHI)))),
+		dot(p.xz, normalize(vec2(1, PHI))));
+	float l = length(p);
+	return l - 1.5 - 0.2 * (1.5 / 2)* cos(min(sqrt(1.01 - b / l)*(PI / 0.25), PI));
+}
+
+// Cylinder standing upright on the xz plane
+float cylinder(vec3 p, float r, float height) {
+	float d = length(p.xz) - r;
+	d = max(d, abs(p.y) - height);
+	return d;
+}
+
+
+float plane(vec3 p) {
+    return box(p-vec3(.0,-1.*.5,.0), vec3(15.,.01,15.), .0);
+}
+
+
+
+float blob2(vec3 p) {
+    p = p*1.7 - vec3(0., 1.,.0);
+    return fblob(p);
+}
+
+float tortor(vec3 p) {
+    mat4 rotmat = rmat(vec3(0.,.5,4.), radians(mx44*360.));
+    p = rot(p, rotmat);
+    p -= vec3(0., .5, 4.);
+    p = xtwist(p, mx11*10.*m1);
+    return smin(length(vec2(length(p.xz) - 1., p.y)) - .2,
+            length(vec2(length(p.xy) - 1., p.z)) - .2, 4.);
+}
+
+float cyl(vec3 p) {
+    p -= vec3(0.,1.5,.0);
+    p = twist(p,mx11);
+    return cylinder(p, 0.5, 1.5);
+}
+
+float scene(vec3 p) {
+    //return min(plane(p), min(cyl(p), min(tor(p), blob2(p))));
+    return min(tortor(p), plane(p));
+}
+
+// taken from http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+vec3 normal(vec3 p) {
+    const vec2 k = vec2(1,-1);
+    return normalize( k.xyy*scene( p + k.xyy*eps ) +
+                      k.yyx*scene( p + k.yyx*eps ) +
+                      k.yxy*scene( p + k.yxy*eps ) +
+                      k.xxx*scene( p + k.xxx*eps ) );
+}
+
+// from https://alaingalvan.tumblr.com/post/79864187609/glsl-color-correction-shaders
+vec3 brightcon(vec3 c, float brightness, float contrast) {
+    return (c - 0.5) * contrast + 0.5 + brightness;
+}
+
+shading get_shading(material m, light l, vec3 p, vec3 n, vec3 ld, vec3 ed) {
+    shading s;
+    float step;
+    s.diffuse = clamp(dot(n,ld), 0., 1.);
+    s.specular = clamp(m.reflection_ratio * pow(dot(normalize(reflect(-ld, n)), ed), m.shininess), 0., 1.);
+
+    s.shadow = 1.;
+    step = 0.01;
+    float ph = 1e10;
+    for (int i = 0; i < MAXSTEPS/3; i++ ) {
+        vec3 shadow_p = ld * step + p;
+        float shadow_d = scene(shadow_p);
+        if (shadow_d < eps) {
+            s.shadow = 0.;
+            break;
+        }
+        if (step > MAXDIST) {
+            break;
+        }
+        float y = pow(shadow_d, 2)/(2.0*ph);
+        float d = sqrt(shadow_d*shadow_d-y*y);
+        s.shadow = min( s.shadow, 32.*d/max(.0,step-y));
+        ph = shadow_d;
+        step += shadow_d;
+    }
+    s.shadow = clamp(s.shadow, 0., 1.);
+    s.aoc = 1.;
+    s.amb = clamp(0.4*n.y+0.5, 0., 1.);
+
+    return s;
+}
+
+void main() {
+    vec3 ro = vec3(cos(time/4.)*4., 3., sin(time/4.)*4.); // ray origin, here also known as 'eye'
+    vec3 lookat = vec3(0.,0.,0.);
+    vec3 fwd = normalize(lookat-ro);
+    vec3 right = normalize(vec3(fwd.z, 0., -fwd.x));
+    vec3 up = normalize(cross(fwd, right));
+    vec2 uv = gl_FragCoord.xy * 2.0 / resolution - 1.0;
+    float aspect = resolution.x/resolution.y;
+    vec3 rd = normalize(1.4*fwd + uv.x*right*aspect + uv.y*up);
+
+    // enviroment
+    l1.color = vec3(1.,1.,1.);
+    l1.position = vec3(sin(time/4.)*4.,2.5,cos(time/4.)*3.);
+    //l1.position = vec3(1.2, 2.0, -1.9);
+    color = vec3(.0);
+    sky = color;
+
+    // raymarch a scene
+    float step = .0;
+    vec3 p;
+    for (int i = 0; i < MAXSTEPS; ++i) {
+        p = ro + (rd * step);
+        float d = scene(p);
+        if (d > MAXDIST) {
+            break;
+        }
+        if (d < eps) {
+            // precompute stuff
+            vec3 n = normal(p);
+            vec3 ld = normalize(l1.position-p);
+            vec3 ed = normalize(ro-p);
+            if (plane(p) < eps) {
+                float checker = mod(floor(p.x)+floor(p.z)-.5, 2.0);
+                mat1.color = vec3(checker);
+                mat1.reflection_ratio = 0.01;
+                mat1.shininess = 3.;
+                s1 = get_shading(mat1, l1, p, n, ld, ed);
+                color = mat1.color * s1.diffuse * s1.shadow/2.;
+                color += s1.specular;
+                color *= s1.aoc;
+                color += mat1.color * s1.amb *.2;
+            }
+            if (cyl(p) < eps) {
+                mat1.color = vec3(1.);
+                mat1.reflection_ratio = 0.01;
+                mat1.shininess = 3.;
+                s1 = get_shading(mat1, l1, p, n, ld, ed);
+                color = mat1.color * s1.diffuse * s1.shadow/2.;
+                color += s1.specular;
+                color *= s1.aoc;
+                color += mat1.color * s1.amb *.2;
+            }
+            if (blob2(p) < eps) {
+                mat1.color = vec3(1.);
+                mat1.reflection_ratio = 0.01;
+                mat1.shininess = 3.;
+                s1 = get_shading(mat1, l1, p, n, ld, ed);
+                color = mat1.color * s1.diffuse * s1.shadow/2.;
+                color += s1.specular;
+                color *= s1.aoc;
+                color += mat1.color * s1.amb *.2;
+            }
+            if (tortor(p) < eps) {
+                mat1.color = vec3(1.);
+                mat1.reflection_ratio = 0.01;
+                mat1.shininess = 3.;
+                s1 = get_shading(mat1, l1, p, n, ld, ed);
+                color = mat1.color * s1.diffuse * s1.shadow/2.;
+                color += s1.specular;
+                color *= s1.aoc;
+                color += mat1.color * s1.amb *.2;
+            }
+            break;
+        }
+        step += d;
+    }
+
+    // Exponential distance fog
+    color = mix(color, 0.8 * sky, 1.0 - exp2(-0.010 * step * step));
+
+    // gamma correction
+    color = pow( color, vec3(1.0/2.2) );
+
+    // send to screen
+    PixelColor = vec4(color, 1.);
+}
