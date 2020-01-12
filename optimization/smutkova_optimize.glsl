f0fc87cae381c5225b402cc100014b69d73cbf5e
gnd <gnd@itchybit.org>
2020-01-13 00:06:19 +0100

    depog smutkova video sources & optimized versions

--- optimization/smutkova_optimize.glsl
+++ optimization/smutkova_optimize.glsl
@@ -1,5 +1,5 @@
 #version 330
-out vec4 PixelColor;
+out vec3 PixelColor;
 uniform vec2 resolution;
 uniform sampler2D sony;
 uniform float time;
@@ -22,13 +22,13 @@ uniform float m8,m9,m10;
 uniform float m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m21,m22,m23,m24,m25,m26,m27,m28,m29,m30;
 uniform float m31,m32,m33,m34,m35,m36,m37,m38,m39,m40,m41,m42,m43,m44,m45,m46,m47,m48,m49,m50;
 uniform float m51,m52,m53,m54,m55,m56,m57,m58,m59;
-float mx11 = m8/127.;
+float mx11 = m8/25.4;
 float mx12 = m9/127.;
 float mx13 = m10/127.;
 float mx14 = m11/127.;
 float mx15 = m12/127.;
 float mx16 = m13/127.;
-float mx21 = m14/127.;
+float mx21 = m14/63.5;
 float mx22 = m15/127.;
 float mx23 = m16/127.;
 float mx24 = m17/127.;
@@ -75,107 +75,84 @@ float mx92 = m57/127.;
 float mx93 = m58/127.;
 float mx94 = m59/127.;
 ///////////////// END OF MIDIMIX ////////////////////
-
-struct light { vec3 position; vec3 color; };
-light l1;
+vec3 p;
 vec3 color;
 
-float vmax(in vec3 p ) {
-    return max(p.x,max(p.y,p.z));
-}
+//float vmax(in vec3 p ) {
+//    return max(p.x,max(p.y,p.z));
+//}
 
-// sony height mapping
-vec3 texcol(vec3 p) {
-    return texture2D(sony, vec2(p.x*.27+.5,p.z*.24+.5)).rgb;
-}
-
-float scene(vec3 p) {
+float scene(vec3 p, float ty) {
     // initial cube
-    float d = vmax(abs(p) - mx31*2.);
+    //float d = vmax(abs(p) - mx31);
+    float d = max(abs(p.x),max(abs(p.y),abs(p.z))) - 1.;
     float s = 1.;
 
     for (int m=0; m<5; m++) {
-         vec3 a = mod(p*s, mx11*5.)-(mx11*2.5);
+         vec3 a = mod(p*s, mx11)-(mx11*.5);
+         vec3 r = 3.*-abs(a) + mx21;
          s *= 3.0;
-         vec3 r = mx21*2. - 3.*abs(a);
          float aa = max(r.y, r.z);
          float ab = max(r.x, r.z);
          float ac = max(r.x, r.y);
          float c = (min(aa, min(ab,ac))-.22)/s;
          d = max(d,-c);
     }
-    return (1.-texcol(p).y)*.01 + d;
+    //return (1.-ty)*.01 + d;
+    return ty * -.01 + .01 + d;
 }
 
 // taken from http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
-vec3 normal(vec3 p) {
+vec3 normal(vec3 p, float ty) {
     const vec2 k = vec2(1,-1);
-    return normalize( k.xyy*scene( p + k.xyy*eps ) +
-                      k.yyx*scene( p + k.yyx*eps ) +
-                      k.yxy*scene( p + k.yxy*eps ) +
-                      k.xxx*scene( p + k.xxx*eps ) );
+    return normalize( k.xyy*scene( k.xyy*eps + p, ty) +
+                      k.yyx*scene( k.yyx*eps + p, ty) +
+                      k.yxy*scene( k.yxy*eps + p, ty));// +
+                      //k.xxx*scene( p + k.xxx*eps , ty) );
 }
 
 void main() {
-    float speed = mx51*1100.;
-    float sintime = sin(time/speed);
-    float costime = cos(time/speed);
+    float speed = time/(mx51*1100.);
+    float sintime = sin(speed);
+    float costime = cos(speed);
     // original eye
     vec3 eye = vec3(sintime*10., sintime*2.+2., costime*10.);
-    //eye = vec3(0,10.,0.);
-
     // original lookat
-    vec3 lookat = vec3(costime*.5, 2.*mx61, sintime*.5);
-
+    vec3 lookat = vec3(costime*.5, .5, sintime*.5);
     vec3 fwd = normalize(lookat-eye);
     vec3 right = normalize(vec3(fwd.z, 0., -fwd.x ));
-    vec3 up = normalize(cross(fwd, right));
-    //float u = gl_FragCoord.x * 2.0 / resolution.x - 1.0;
-    //float v = gl_FragCoord.y * 2.0 / resolution.y - 1.0;
+    //vec3 up = normalize(cross(fwd, right));
+    vec3 up = vec3(0.,1.,0.);
     vec2 uv = gl_FragCoord.xy * 2.0 / resolution - 1.0;
     float aspect = resolution.x/resolution.y;
-    vec3 ro = eye;
-    vec3 rd = normalize(fwd * (mx41*150.) + right * (uv.x*aspect) + up * uv.y);
-
+    //vec3 ro = eye;
+    vec3 rd = normalize(fwd * (mx41*150.) + right * (uv.x*aspect) + up*uv.y);
     // enviroment
-    l1.color = vec3(1.,1.,1.);
-    l1.position = vec3(sin(time/4.)*4.,2.5,cos(time/4.)*3.);
-    //l1.position = vec3(1.2, 2.0, -1.9);
+    vec3 lpos = vec3(sin(time/4.)*4.,2.5,cos(time/4.)*3.);
     color = vec3(.0);
 
     // raymarch a scene
     float step = .0;
-    vec3 p, p_refr;
-    for (int i = 0; i < MAXSTEPS; ++i) {
-        p = rd * step + ro;
-        float d = scene(p);
-        if (d > MAXDIST) {
-            break;
-        }
+    for (int i = 0; i < MAXSTEPS; i+=2) {
+        p = rd * step + eye;
+        vec3 texxcol = texture(sony, vec2(p.x*.27+.5,p.z*.24+.5)).rgb;
+        float d = scene(p, texxcol.y);
         if (d < eps) {
             // precompute stuff
-            vec3 n = normal(p);
-            vec3 ld = normalize(l1.position-p);
-            vec3 ed = normalize(ro-p);
-            vec3 col = vec3(.4);
-            // no need to distinguish objects here
-            color = col * clamp(dot(n,ld), 0., 1.);
-            color += col * clamp(dot(normalize(reflect(-ld, n)), ed), 0., 1.2);
-            color += col * clamp(.4*n.y+.5, 0., .01);
+            vec3 n = normal(p, texxcol.y);
+            vec3 ld = lpos-p;
+            vec3 ed = eye-p;
+            color = vec3(clamp(dot(n,ld), 0.,.4)
+                    + clamp(dot(reflect(-ld, n), ed),0.,.48)
+                    + n.y*.4+.01);
             // add step fog
-            color += vec3(float(i)/float(MAXSTEPS),0.,0.)*2.;
-            color /= texcol(p) * .9;
+            color.r += float(i)/float(MAXSTEPS)*2.;
+            color /= texxcol;;
             break;
         }
         step += d;
     }
 
-    // Exponential distance fog
-    color = mix(color, 0.8 * vec3(0.), 1.0 - exp2(-.003 * step * step * step));
-
-    // gamma correction
-    color = pow( color, vec3(1.0/2.2) );
-
-    // send to screen
-    PixelColor = vec4(color, 1.);
+    // Exponential distance fog FIXME
+    PixelColor = mix(color, vec3(0.), .6 - exp2(-.9 * step));
 }
--- optimization/smutkova_optimize_uniforms.glsl
+++ optimization/smutkova_optimize_uniforms.glsl
@@ -1,8 +1,10 @@
 #version 330
-out vec4 PixelColor;
+out vec3 PixelColor;
 uniform vec2 resolution;
 uniform sampler2D sony;
-uniform float time;
+//uniform float time;
+uniform float stime;
+uniform float ctime;
 #define MAXSTEPS 256
 #define MAXDIST 10.0
 #define eps 0.00001
@@ -22,13 +24,15 @@ uniform float m8,m9,m10;
 uniform float m11,m12,m13,m14,m15,m16,m17,m18,m19,m20,m21,m22,m23,m24,m25,m26,m27,m28,m29,m30;
 uniform float m31,m32,m33,m34,m35,m36,m37,m38,m39,m40,m41,m42,m43,m44,m45,m46,m47,m48,m49,m50;
 uniform float m51,m52,m53,m54,m55,m56,m57,m58,m59;
-float mx11 = m8/127.;
+//float mx11 = m8/25.4;
+uniform float mx11;
 float mx12 = m9/127.;
 float mx13 = m10/127.;
 float mx14 = m11/127.;
 float mx15 = m12/127.;
 float mx16 = m13/127.;
-float mx21 = m14/127.;
+//float mx21 = m14/63.5;
+uniform float mx21;
 float mx22 = m15/127.;
 float mx23 = m16/127.;
 float mx24 = m17/127.;
@@ -40,13 +44,16 @@ float mx33 = m22/127.;
 float mx34 = m23/127.;
 float mx35 = m24/127.;
 float mx36 = m25/127.;
-float mx41 = m26/127.;
+//float mx41 = m26/127.;
+uniform float mx41;
 float mx42 = m27/127.;
 float mx43 = m28/127.;
 float mx44 = m29/127.;
 float mx45 = m30/127.;
 float mx46 = m31/127.;
-float mx51 = m32/127.;
+//float mx51 = m32/127.;
+uniform float sintime;
+uniform float costime;
 float mx52 = m33/127.;
 float mx53 = m34/127.;
 float mx54 = m35/127.;
@@ -75,107 +82,97 @@ float mx92 = m57/127.;
 float mx93 = m58/127.;
 float mx94 = m59/127.;
 ///////////////// END OF MIDIMIX ////////////////////
-
-struct light { vec3 position; vec3 color; };
-light l1;
+vec3 p;
 vec3 color;
 
-float vmax(in vec3 p ) {
-    return max(p.x,max(p.y,p.z));
+//float vmax(in vec3 p ) {
+//    return max(p.x,max(p.y,p.z));
+//}
+
+float fogFactorExp2(const float dist, const float density) {
+  const float LOG2 = -1.442695;
+  float d = density * dist;
+  return 1. - clamp(exp2(d * d * LOG2), 0.0, 1.0);
 }
 
-// sony height mapping
-vec3 texcol(vec3 p) {
-    return texture2D(sony, vec2(p.x*.27+.5,p.z*.24+.5)).rgb;
-}
-
-float scene(vec3 p) {
+float scene(vec3 p, float ty) {
     // initial cube
-    float d = vmax(abs(p) - mx31*2.);
+    //float d = vmax(abs(p) - mx31);
+    float d = max(abs(p.x),max(abs(p.y),abs(p.z))) - 1.;
     float s = 1.;
 
     for (int m=0; m<5; m++) {
-         vec3 a = mod(p*s, mx11*5.)-(mx11*2.5);
+         vec3 a = mod(p*s, mx11)-(mx11*.5);
+         vec3 r = 3.*-abs(a) + mx21;
          s *= 3.0;
-         vec3 r = mx21*2. - 3.*abs(a);
          float aa = max(r.y, r.z);
          float ab = max(r.x, r.z);
          float ac = max(r.x, r.y);
          float c = (min(aa, min(ab,ac))-.22)/s;
          d = max(d,-c);
     }
-    return (1.-texcol(p).y)*.01 + d;
+    //return (1.-ty)*.01 + d;
+    return ty * -.01 + .01 + d;
 }
 
 // taken from http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
-vec3 normal(vec3 p) {
+vec3 normal(vec3 p, float ty) {
     const vec2 k = vec2(1,-1);
-    return normalize( k.xyy*scene( p + k.xyy*eps ) +
-                      k.yyx*scene( p + k.yyx*eps ) +
-                      k.yxy*scene( p + k.yxy*eps ) +
-                      k.xxx*scene( p + k.xxx*eps ) );
+    return normalize( k.xyy*scene( k.xyy*eps + p, ty) +
+                      k.yyx*scene( k.yyx*eps + p, ty) +
+                      k.yxy*scene( k.yxy*eps + p, ty) +
+                      k.xxx*scene( k.xxx*eps + p, ty) );
 }
 
 void main() {
-    float speed = mx51*1100.;
-    float sintime = sin(time/speed);
-    float costime = cos(time/speed);
+    /*
+    float speed = time/(mx51*1100.);
+    float sintime = sin(speed);
+    float costime = cos(speed);
+    */
     // original eye
     vec3 eye = vec3(sintime*10., sintime*2.+2., costime*10.);
-    //eye = vec3(0,10.,0.);
-
     // original lookat
-    vec3 lookat = vec3(costime*.5, 2.*mx61, sintime*.5);
-
+    vec3 lookat = vec3(costime*.5, .5, sintime*.5);
     vec3 fwd = normalize(lookat-eye);
     vec3 right = normalize(vec3(fwd.z, 0., -fwd.x ));
-    vec3 up = normalize(cross(fwd, right));
-    //float u = gl_FragCoord.x * 2.0 / resolution.x - 1.0;
-    //float v = gl_FragCoord.y * 2.0 / resolution.y - 1.0;
+    //vec3 up = normalize(cross(fwd, right));
+    vec3 up = vec3(0.,1.,0.);
     vec2 uv = gl_FragCoord.xy * 2.0 / resolution - 1.0;
     float aspect = resolution.x/resolution.y;
-    vec3 ro = eye;
-    vec3 rd = normalize(fwd * (mx41*150.) + right * (uv.x*aspect) + up * uv.y);
-
+    //vec3 ro = eye;
+    //vec3 rd = normalize(fwd * (mx41*150.) + right * (uv.x*aspect) + up*uv.y);
+    vec3 rd = normalize(fwd * mx41 + right * (uv.x*aspect) + up*uv.y);
     // enviroment
-    l1.color = vec3(1.,1.,1.);
-    l1.position = vec3(sin(time/4.)*4.,2.5,cos(time/4.)*3.);
-    //l1.position = vec3(1.2, 2.0, -1.9);
+    //vec3 lpos = vec3(sin(time/4.)*4.,2.5,cos(time/4.)*3.);
+    vec3 lpos = vec3(stime*4.,2.5,ctime*3.);
     color = vec3(.0);
 
     // raymarch a scene
     float step = .0;
-    vec3 p, p_refr;
-    for (int i = 0; i < MAXSTEPS; ++i) {
-        p = rd * step + ro;
-        float d = scene(p);
-        if (d > MAXDIST) {
-            break;
-        }
+    for (int i = 0; i < MAXSTEPS; i+=2) {
+        p = rd * step + eye;
+        vec3 texxcol = texture(sony, vec2(p.x*.27+.5,p.z*.24+.5)).rgb;
+        float d = scene(p, texxcol.y);
         if (d < eps) {
             // precompute stuff
-            vec3 n = normal(p);
-            vec3 ld = normalize(l1.position-p);
-            vec3 ed = normalize(ro-p);
-            vec3 col = vec3(.4);
-            // no need to distinguish objects here
-            color = col * clamp(dot(n,ld), 0., 1.);
-            color += col * clamp(dot(normalize(reflect(-ld, n)), ed), 0., 1.2);
-            color += col * clamp(.4*n.y+.5, 0., .01);
-            // add step fog
-            color += vec3(float(i)/float(MAXSTEPS),0.,0.)*2.;
-            color /= texcol(p) * .9;
+            vec3 n = normal(p, texxcol.y);
+            vec3 ld = lpos-p;
+            vec3 ed = eye-p;
+            color = vec3(clamp(dot(n,ld), 0.,.4)
+                    + clamp(dot(reflect(-ld, n), ed),0.,.48)
+                    + n.y*.4+.01);
+            // add step glow
+            color.r += float(i)/float(MAXSTEPS)*.9;
+            color /= texxcol*texxcol*texxcol;
             break;
         }
         step += d;
     }
 
-    // Exponential distance fog
-    color = mix(color, 0.8 * vec3(0.), 1.0 - exp2(-.003 * step * step * step));
+    // Exponential distance fog FIXME
+    //PixelColor = mix(color, vec3(0.), .6 - exp2(-.9 * step));
+    PixelColor = color;
+    PixelColor = mix(color, vec3(0.), fogFactorExp2(step, .14));
 
-    // gamma correction
-    color = pow( color, vec3(1.0/2.2) );
-
-    // send to screen
-    PixelColor = vec4(color, 1.);
 }
